d81c12e68cc992c63cc6d73b17a70aae29290fb4
Camilo Mora <kalufau@hotmil.com>
2021-06-21 18:07:07 -0500

    add page home

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,18 @@ import '@camiloamora/components/styles/globals.css'
 import '@camiloamora/components/styles/tokens.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div
+    style={{
+      width: 375,
+      padding: "40px 30px",
+      border: "1px solid rebeccapurple",
+      margin: "0 auto",
+       }}
+    >
+    <Component {...pageProps} />
+  </div>
+  )
 }
 
 export default MyApp
---
+++ pages/home.js
@@ -0,0 +1,50 @@
+import { Avatar, Heading, Spacer, Card, Icon } from '@camiloamora/components'
+
+export default function Home() {
+
+  return <div
+  style={{ display: 'flex',
+  flexDirection: 'column' }}>
+
+    <div style={{ display: "flex" }}>
+      <Avatar src="http://placeimg.com/200/200/people"></Avatar>
+      <Spacer.Vertical size="xs" />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <Heading size="lg">Buenos días, Camilo</Heading>
+        <Heading size="md" color="primary">¿Cómo quieres empezar?</Heading>
+      </div>
+    </div>
+    <Spacer.Horizontal size="lg" />
+    <Card color="secondary" size="lg">
+          Buscar un espacio para trabajar
+    </Card>
+    <Spacer.Horizontal size="md" />
+    <Card color="primary" size="lg">
+        Iniciar una sesión de productividad
+    </Card>
+    <Spacer.Horizontal size="lg" />
+    <Card size="sm">
+        <Icon type="angle-down" hasBackground>
+          <span>
+            María ha hecho check-in en Factoria
+          </span>
+        </Icon>
+    </Card>
+    <Spacer.Horizontal size="xs" />
+    <Card size="sm">
+        <Icon type="angle-down" hasBackground>
+          <span>
+            María ha hecho check-in en Factoria
+          </span>
+        </Icon>
+    </Card>
+    <Spacer.Horizontal size="xs" />
+    <Card size="sm">
+        <Icon type="angle-down" hasBackground>
+          <span>
+            María ha hecho check-in en Factoria
+          </span>
+        </Icon>
+    </Card>
+  </div>
+}
